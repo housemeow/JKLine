c542648010de38b7a0c7e41ac0beea3f3c4a2063
Jay-Liao <j9950314@gmail.com>
2014-04-24 14:32:36 +0800

    message apis

--- js/JKLineDB.js
+++ js/JKLineDB.js
@@ -3,7 +3,7 @@ app.factory('JKLineDB', function($window, PhoneGap) {
     PhoneGap.ready(function() {
         db = $window.sqlitePlugin.openDatabase({name: "JKLineDB"});
         db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS Preference(id INTEGER, name TEXT, state TEXT)", []);
+            tx.executeSql("CREATE TABLE IF NOT EXISTS Preference(mid INTEGER, name TEXT, state TEXT)", []);
             tx.executeSql("CREATE TABLE IF NOT EXISTS Friends(mid INTEGER, name TEXT, state TEXT)", []);
             tx.executeSql("CREATE TABLE IF NOT EXISTS Invitations(mid INTEGER, name TEXT)", []);
             tx.executeSql("CREATE TABLE IF NOT EXISTS MessageLog(mid INTEGER, message TEXT, timeStamp DATETIME, messageState INTEGER )", []);
@@ -15,8 +15,9 @@ app.factory('JKLineDB', function($window, PhoneGap) {
     		PhoneGap.ready(function() {
 	            db.transaction(function(tx) {
 	                tx.executeSql("DELETE FROM Preference WHERE 1");
-	                tx.executeSql("INSERT INTO Preference(id, name, state) VALUES (?, ?, ?)",
-	                [member.id, member.name, member.state],
+	                tx.executeSql(
+	                		"INSERT INTO Preference(mid, name, state) VALUES (?, ?, ?)",
+	                		[member.mid, member.name, member.state],
 	                    function(tx, res) {
 	                		friend.id = res.insertId;
 	                        (onSuccess || angular.noop)();
@@ -56,6 +57,41 @@ app.factory('JKLineDB', function($window, PhoneGap) {
         	PhoneGap.ready(function() {
         		db.transaction(function(tx) {
         			tx.executeSql("SELECT * FROM Invitations", [],
+	        			onSuccess,
+        				onError
+    				);
+            	});
+            });
+        },
+        
+        getTheLatestMessageLog: function (friend, onSuccess, onError) {
+        	PhoneGap.ready(function() {
+        		db.transaction(function(tx) {
+        			tx.executeSql("SELECT * FROM MessageLog,Friend where (MessageLog.timeStamp = (select max(timeStamp) as latestTime from MessageLog where mid = ?)) and (friend.mid = MessageLog.mid)", [friend.mid],
+	        			onSuccess,
+        				onError
+    				);
+            	});
+            });
+        },
+        
+        getMessageLog: function (friend, onSuccess, onError) {
+        	PhoneGap.ready(function() {
+        		db.transaction(function(tx) {
+        			tx.executeSql("SELECT * FROM MessageLog WHERE mid = ?", [friend.mid],
+	        			onSuccess,
+        				onError
+    				);
+            	});
+            });
+        },
+        
+        saveMessage: function (messageLog, onSuccess, onError) {
+        	PhoneGap.ready(function() {
+        		db.transaction(function(tx) {
+        			tx.executeSql(
+	                		"INSERT INTO MessageLog(mid, message, timeStamp, messageState) VALUES (?, ?, ?, ?)",
+	                		[messageLog.mid, messageLog.message, messageLog.timeStamp, message.messageState],
 	        			onSuccess,
         				onError
     				);
