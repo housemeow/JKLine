1aa1e09814ee7538c9637eb3a914a0e456228efa
Kai <housemeow@gmail.com>
2014-04-24 22:57:14 +0800

    remove chatroom footer

--- js/JKLineDB.js
+++ js/JKLineDB.js
@@ -176,3 +176,17 @@ app.factory('JKLineDB', function($window, PhoneGap) {
 		//JKLineReadAllMessageLog();
     };
 });
+
+
+app.factory('SettingManager', function($window) {
+	if (!$window.localStorage['host'])
+		$window.localStorage['host'] = "{}";
+	return {
+		setHost: function(host) {
+			$window.localStorage['host'] = JSON.stringify(host);
+		},
+		getHost: function() {
+			return JSON.parse($window.localStorage['host']);
+		}
+	};
+});
